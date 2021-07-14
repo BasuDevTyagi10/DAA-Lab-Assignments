82221291868878673e6b4f1d1a7cb60dedf4ac99
Basudev Tyagi <tyagi101basudev@gmail.com>
2021-07-14 13:27:19 +0530

    Week0: input and java file added

---
+++ Week0/linear_search.java
@@ -0,0 +1,23 @@
+package Week0;
+
+/**
+ Question-1:
+ Given an array of nonnegative integers, design a linear algorithm and implement it using a program
+ to find whether a given key element is present in the array or not.
+ Also, find the total number of comparisons for each input case.
+ (Time Complexity = O(n), where n is the size of input).
+ Input Format:
+ n - no. of test cases
+ next n lines:
+ x - size of array
+ a1 a2 a3 .... asize - x spaced non-negative integers
+ key - key element to find
+ Output Format:
+ YES or NO - if key is present or absent
+ comparisions - no. of comparisons made
+ */
+public class linear_search {
+    public static void main(String[] args) {
+        
+    }
+}

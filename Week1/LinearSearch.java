d51a1ea7c6a540f6e8b7707740daff1495608b32
Basudev Tyagi <tyagi101basudev@gmail.com>
2021-07-17 10:56:12 +0530

    program logic added to LinearSearch.java

--- Week1/LinearSearch.java
+++ Week1/LinearSearch.java
@@ -7,5 +7,23 @@ import java.util.*;
 public class LinearSearch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0){
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+            int key=sc.nextInt();
+            int comparisions=0; boolean isPresent=false;
+            for(int i=0;i<n;i++){
+                comparisions++;
+                if(arr[i]==key){
+                    isPresent=true;
+                    break;
+                }
+            }
+            if(isPresent) System.out.println("Present "+comparisions);
+            else System.out.println("Not Present "+comparisions);
+        }
+        sc.close();
     }
 }

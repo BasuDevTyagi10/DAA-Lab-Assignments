a2e696e4b35ca27be75168c4a8ad1987154dcad6
Basudev Tyagi <tyagi101basudev@gmail.com>
2021-07-22 13:50:33 +0530

    program logic added to Problem3_W2.java

--- Week2/Problem3_W3.java
+++ Week2/Problem3_W3.java
@@ -8,7 +8,22 @@ import java.util.Scanner;
 public class Problem3_W3 {
     public static void main(String[] args) throws IOException{
         try {
-            Scanner sc = new Scanner(new File("./Week2/input2.txt"));
+            Scanner sc = new Scanner(new File("./Week2/input3.txt"));
+            int t = sc.nextInt();
+            while(t-- > 0){
+                int n = sc.nextInt();
+                int arr[] = new int[n];
+                int i,j;
+                for(i=0;i<n;i++) arr[i] = sc.nextInt();
+                int K = sc.nextInt();
+                int count = 0;
+                for(i=0;i<n-1;i++){
+                    for(j=i+1;j<n;j++)
+                        if(arr[i] - arr[j] == K || arr[j] - arr[i] == K)
+                            count++;
+                }
+                System.out.println(count);
+            }
             sc.close();
         } catch (FileNotFoundException e){
             System.out.println("File Not Found");

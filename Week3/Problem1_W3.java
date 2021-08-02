474027d7b329bb3cbbabd20c82642e343f2b19b3
Basudev Tyagi <tyagi101basudev@gmail.com>
2021-08-02 15:02:47 +0530

    program logic added to Problem1_W3.java

--- Week3/Problem1_W3.java
+++ Week3/Problem1_W3.java
@@ -8,6 +8,28 @@ public class Problem1_W3 {
     public static void main(String[] args) {
         try{
             Scanner sc = new Scanner(new File("./Week3/input1.txt"));
+            int t = sc.nextInt();
+            while(t-- > 0){
+                int n = sc.nextInt();
+                int arr[] = new int[n];
+                int i,j,key,comparisions=0,shifts=0;
+                for(i=0;i<n;i++) arr[i] = sc.nextInt();
+                for(i=1;i<n;i++){
+                    key = arr[i];
+                    j = i-1;
+                    while(j>=0 && arr[j]>key){
+                        comparisions++;
+                        arr[j+1] = arr[j];
+                        shifts++;
+                        j--;
+                    }
+                    arr[j+1] = key;
+                    shifts++;
+                }
+                for(i=0;i<n;i++) System.out.print(arr[i]+" ");
+                System.out.println("\ncomparisions = "+comparisions);
+                System.out.println("shifts = "+shifts);
+            }
             sc.close();
         } catch(FileNotFoundException e){
             System.out.println("File Not Found");

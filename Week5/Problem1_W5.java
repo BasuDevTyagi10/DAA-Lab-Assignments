59650d3154e45d55d8f41964a29dd5b435a33922
Basudev Tyagi <tyagi101basudev@gmail.com>
2021-08-20 00:20:49 +0530

    Week5: input and java files added

---
+++ Week5/Problem1_W5.java
@@ -0,0 +1,17 @@
+package Week5;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Problem1_W5 {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(new File("./Week5/input1.txt"));
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found");
+        }
+    }
+    
+}

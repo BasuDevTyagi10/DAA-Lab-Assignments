c2b62fac752440ade5d9ee8596bec3556588b67c
Basudev Tyagi <tyagi101basudev@gmail.com>
2021-08-20 23:35:32 +0530

    program logic added to Problem3_W5.java

--- Week5/Problem3_W5.java
+++ Week5/Problem3_W5.java
@@ -8,6 +8,24 @@ public class Problem3_W5 {
     public static void main(String[] args) {
         try {
             Scanner sc = new Scanner(new File("./Week5/input3.txt"));
+            int m = sc.nextInt();
+            int arr1[] = new int[m];
+            int i,j;
+            for(i=0;i<m;i++) arr1[i] = sc.nextInt();
+            int n = sc.nextInt();
+            int arr2[] = new int[n];
+            for(i=0;i<n;i++) arr2[i] = sc.nextInt();
+            i=0; j=0;
+            while (i<m && j<n) {
+                if(arr1[i]<arr2[j])
+                    i++;
+                else if(arr2[j] < arr1[i])
+                    j++;
+                else{
+                    System.out.print(arr2[j] + " ");
+                    i++; j++;
+                }
+            }
             sc.close();
         } catch (FileNotFoundException e) {
             System.out.println("File Not Found");

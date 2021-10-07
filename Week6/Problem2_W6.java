3d0a6641ec758f31d53d5beeea000c57b4d37f89
Basudev Tyagi <tyagi101basudev@gmail.com>
2021-10-07 10:55:26 +0530

    Fixed input and added starter code to java file for Problem2_W6

--- Week6/Problem2_W6.java
+++ Week6/Problem2_W6.java
@@ -5,9 +5,19 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Problem2_W6 {
+    static int v = 0;
+    public static boolean isBipartite(int g[][], int s) {
+        return false;
+    }
     public static void main(String[] args) {
         try {
             Scanner sc = new Scanner(new File("./Week6/input2.txt"));
+            int t = sc.nextInt();
+            while(t-- > 0){
+                v = sc.nextInt();
+                int g[][] = new int[v][v];
+                System.out.println(isBipartite(g,0)?"YES":"NO");
+            }
             sc.close();
         } catch (FileNotFoundException e) {
             System.out.println("File Not Found");
